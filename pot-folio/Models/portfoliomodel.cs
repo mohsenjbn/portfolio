25d48626c76dae5c49987020088268cf0a70be2a
mohsen <mhsnj342@gmail.com>
2021-09-26 17:00:00 +0330

    Add model portfolio

---
+++ pot-folio/Models/portfoliomodel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pot_folio.Models
+{
+    public class portfoliomodel
+    {
+        public int Id { get; set; }
+        public string subject { get; set; }
+        public string image { get; set; }
+        public string Describtion { get; set; }
+    }
+}
--- pot-folio/ViewComponent/portfolio.cs
+++ pot-folio/ViewComponent/portfolio.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
+using pot_folio.Models;
 
 namespace pot_folio.ViewComponent
 {
@@ -12,7 +13,33 @@ namespace pot_folio.ViewComponent
 
         public ViewViewComponentResult Invoke()
         {
-            return View("/Views/Home/component/portfolio.cshtml");
+            var portfolios=new List<portfoliomodel>()
+            {
+                new portfoliomodel()
+                {
+                    Id = 1,
+                  subject = "سعید هویجی",
+                  Describtion = "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the",
+                  image = "work-1.jpg"
+                },
+                new portfoliomodel()
+                {
+                    Id = 2,
+                    subject = "Black & Gold Paint Face",
+                    Describtion = "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the",
+                    image = "work-2.jpg"
+                },
+                new portfoliomodel()
+                {
+                    Id = 3,
+                    subject = "Black & Gold Paint Face",
+                    Describtion = "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the",
+                    image = "work-3.jpg"
+                }
+
+            };
+            
+            return View("/Views/Home/component/portfolio.cshtml",portfolios);
         }
     }
 }
