54ece72f7c4d37980631d68af132c1d927359787
mohsen <mhsnj342@gmail.com>
2021-09-26 16:38:46 +0330

    Add portfolio in view component

---
+++ pot-folio/ViewComponent/portfolio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace pot_folio.ViewComponent
+{
+    public class portfolio : Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+
+        public ViewViewComponentResult Invoke()
+        {
+            return View("/Views/Home/component/portfolio.cshtml");
+        }
+    }
+}
